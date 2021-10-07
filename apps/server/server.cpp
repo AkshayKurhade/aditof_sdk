b2bd661e2cae55adcc77e78d707512e98ab3198c
Akshay Kurhade <56202295+AkshayKurhade@users.noreply.github.com>
2021-10-07 16:35:09 -0400

    Update server.cpp

--- apps/server/server.cpp
+++ apps/server/server.cpp
@@ -243,7 +243,7 @@ int main(int argc, char *argv[]) {
     struct lws_context_creation_info info;
     memset(&info, 0, sizeof(info));
 
-    info.port = 5021;
+    info.port = 5051;
     info.protocols = protocols;
     info.gid = -1;
     info.uid = -1;
